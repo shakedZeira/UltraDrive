021433173aa1e2618dedcb2ac1a5568524b85464
UltraDrive Dev <dev@ultradrive.local>
2026-09-11 10:31:23 +0300

    feat: add test circuit with checkpoints and player car

---
+++ scripts/test_circuit.gd
@@ -0,0 +1,36 @@
+extends Node3D
+
+func _ready() -> void:
+    # Build an oval track
+    var builder := TrackBuilder.new()
+    builder.road_width = 12.0
+    add_child(builder)
+
+    var points: Array[Vector3] = []
+    var radius := 60.0
+    var segments := 32
+    for i in range(segments):
+        var angle := TAU * i / segments
+        points.append(Vector3(cos(angle) * radius, 0.1, sin(angle) * radius * 0.6))
+    builder.build_track(points)
+
+    # Add checkpoints around the track
+    for i in range(8):
+        var angle := TAU * i / 8
+        var cp := Checkpoint.new()
+        cp.index = i
+        cp.position = Vector3(cos(angle) * radius, 0.5, sin(angle) * radius * 0.6)
+
+        # Give the checkpoint a detection volume (Checkpoint Area3D has no shape by default)
+        var area_shape := BoxShape3D.new()
+        area_shape.size = Vector3(8.0, 5.0, 8.0)
+        var area_col := CollisionShape3D.new()
+        area_col.shape = area_shape
+        cp.add_child(area_col)
+
+        cp.body_entered.connect(_on_checkpoint_body_entered.bind(cp.index))
+        add_child(cp)
+
+func _on_checkpoint_body_entered(body: Node3D, index: int) -> void:
+    if body is VehiclePhysics:
+        print("[Track] Checkpoint %d passed by %s" % [index, body.name])
