d19f4f17b5c30af8e8dc7f8556914a796fd39d0c
UltraDrive Dev <dev@ultradrive.local>
2026-09-11 18:52:14 +0300

    fix: boot straight into the main menu

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,4 +1,5 @@
 extends Node3D
 
 func _ready() -> void:
-    print("[UltraDrive] Main scene loaded")
+    print("[UltraDrive] Main scene loaded – transitioning to main menu")
+    SceneTransition.flash_to_scene("res://scenes/ui/main_menu.tscn")
